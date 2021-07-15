74b8dafe17b43fa19fd9b020a55bb97b791b3932
Cristian Cobo <cristian.cobo1@u.icesi.edu.co>
2021-07-15 18:14:51 -0500

    creación configuración combo

--- microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -1,10 +1,26 @@
 package com.ceiba.configuracion;
 
+import com.ceiba.combo.puerto.repositorio.RepositorioCombo;
+import com.ceiba.combo.servicio.ServicioActualizarCombo;
+import com.ceiba.combo.servicio.ServicioCrearCombo;
+import com.ceiba.combo.servicio.ServicioEliminarCombo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
 
-
+    @Bean
+    public ServicioCrearCombo servicioCrearCombo(RepositorioCombo repositorioCombo){
+        return new ServicioCrearCombo(repositorioCombo);
+    }
+    @Bean
+    public ServicioActualizarCombo servicioActualizarCombo(RepositorioCombo repositorioCombo){
+        return new ServicioActualizarCombo(repositorioCombo);
+    }
+    @Bean
+    public ServicioEliminarCombo servicioEliminarCombo(RepositorioCombo repositorioCombo){
+        return new ServicioEliminarCombo(repositorioCombo);
+    }
 
 }
