c044d886f3f7407a0d4a07e0366cc49d8a9a6026
Cristian Cobo <cristian.cobo1@u.icesi.edu.co>
2021-07-15 19:02:04 -0500

    creación infracestructura-adaptador reserva

---
+++ microservicio/infraestructura/src/main/java/com/ceiba/reserva/adaptador/dao/MapeoReserva.java
@@ -0,0 +1,30 @@
+package com.ceiba.reserva.adaptador.dao;
+
+
+import com.ceiba.infraestructura.jdbc.MapperResult;
+import com.ceiba.reserva.modelo.dto.DtoReserva;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class MapeoReserva implements RowMapper<DtoReserva>, MapperResult {
+    @Override
+    public DtoReserva mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+        Long id = resultSet.getLong("id");
+        Long combo_id = resultSet.getLong("combo_id");
+        double precioFinalReserva = resultSet.getDouble("precio");
+        LocalDateTime fechaCreacionReserva = extraerLocalDateTime(resultSet,"fecha_creacion");
+        LocalDateTime fechaReserva = extraerLocalDateTime(resultSet,"fecha_reserva");
+        LocalDateTime fechaExpiracionReserva = extraerLocalDateTime(resultSet,"fecha_expiracion");
+        String nombrePersonaReserva = resultSet.getString("nombre_persona");
+        String idPersonaReserva = resultSet.getString("id_persona");
+        String telefonoPersonaReserva = resultSet.getString("telefono_persona");
+        String direccionPersonaReserva = resultSet.getString("direccion_persona");
+
+
+        return new DtoReserva(id, combo_id,precioFinalReserva, fechaCreacionReserva,fechaReserva,fechaExpiracionReserva,nombrePersonaReserva,idPersonaReserva,telefonoPersonaReserva,direccionPersonaReserva);
+
+    }
+}
