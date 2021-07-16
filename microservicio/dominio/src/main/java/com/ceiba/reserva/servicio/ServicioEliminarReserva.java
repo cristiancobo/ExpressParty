fd528dadecfd0b19b46a9c6b93da180d6f1b30fc
Cristian Cobo <cristian.cobo1@u.icesi.edu.co>
2021-07-16 10:27:19 -0500

    refactor servicio eliminar reserva y excepciones pertinentes

--- microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioEliminarReserva.java
+++ microservicio/dominio/src/main/java/com/ceiba/reserva/servicio/ServicioEliminarReserva.java
@@ -1,5 +1,7 @@
 package com.ceiba.reserva.servicio;
 
+import com.ceiba.dominio.excepcion.ExcepcionNoExisteReserva;
+import com.ceiba.dominio.excepcion.ExcepcionTiempoExcedido;
 import com.ceiba.reserva.modelo.entidad.Reserva;
 import com.ceiba.reserva.puerto.repositorio.RepositorioReserva;
 
@@ -9,6 +11,9 @@ import java.time.temporal.ChronoUnit;
 public class ServicioEliminarReserva {
 
     private final RepositorioReserva repositorioReserva;
+    private final String RESERVA_NO_EXISTE = "La reserva que intenta eliminar no existe";
+    private final String TIEMPO_EXCEDIDO = "El tiempo para realizar la cancelación excedió";
+    private final int TIEMPO_MINUTOS_MAXIMO_CANCELACION_RESERVA = 30;
 
 
     public ServicioEliminarReserva(RepositorioReserva repositorioReserva) {
@@ -16,24 +21,25 @@ public class ServicioEliminarReserva {
     }
 
     public void ejecutar(Long id) {
-
-        if(estaVigenteParaCancelarReserva(id)){
+             verficiarExistenciaReserva(id);
+            estaVigenteParaCancelarReserva(id);
             this.repositorioReserva.eliminar(id);
-        }else{
-            System.out.println("EJEJEJE");
-        }
-
     }
 
-    public boolean estaVigenteParaCancelarReserva(Long id){
+    public void estaVigenteParaCancelarReserva(Long id){
         LocalDateTime fechaActual = LocalDateTime.now();
         LocalDateTime fechaReservacion = this.repositorioReserva.encontrarFechaCreacionReserva(id);
         LocalDateTime tiempo = LocalDateTime.from( fechaReservacion );
         long minutosTranscurridos = tiempo.until( fechaActual, ChronoUnit.MINUTES );
-        if(minutosTranscurridos < 30 ){
-            return true;
+        if(!(minutosTranscurridos < TIEMPO_MINUTOS_MAXIMO_CANCELACION_RESERVA) ){
+            throw new ExcepcionTiempoExcedido(TIEMPO_EXCEDIDO);
         }
-        return false;
+
+    }
+    public void verficiarExistenciaReserva(Long id){
+        if(!this.repositorioReserva.existe(id)){
+            throw new ExcepcionNoExisteReserva(RESERVA_NO_EXISTE);
+        }
     }
 
 }
