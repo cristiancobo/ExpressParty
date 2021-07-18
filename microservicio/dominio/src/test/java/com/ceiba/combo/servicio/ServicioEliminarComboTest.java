e53df28bcf5769ce93689e472203d2d66222c8f1
Cristian Cobo <cristian.cobo1@u.icesi.edu.co>
2021-07-17 20:55:26 -0500

    pruebas servicio eliminar y actualizar combo

--- microservicio/dominio/src/test/java/com/ceiba/combo/servicio/ServicioEliminarComboTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/combo/servicio/ServicioEliminarComboTest.java
@@ -1,4 +1,19 @@
 package com.ceiba.combo.servicio;
 
+import com.ceiba.combo.modelo.entidad.Combo;
+import com.ceiba.combo.puerto.repositorio.RepositorioCombo;
+import com.ceiba.combo.servicio.testdatabuilder.ComboTestDataBuilder;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class ServicioEliminarComboTest {
+
+    @Test
+    public void validarEliminarComboCorrectamente(){
+        RepositorioCombo repositorioCombo = Mockito.mock(RepositorioCombo.class);
+        Mockito.doNothing().when(repositorioCombo).eliminar(Mockito.anyLong());
+        ServicioEliminarCombo servicioEliminarCombo = new ServicioEliminarCombo(repositorioCombo);
+        servicioEliminarCombo.ejecutar(1L);
+        Mockito.verify(repositorioCombo,Mockito.times(1)).eliminar(Mockito.anyLong());
+    }
 }
